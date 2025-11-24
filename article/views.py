74c7f68b52fdd4b119fcce8e418dc1113853aa01
RomanVetrov <romanvikernes@gmail.com>
2025-11-24 20:52:33 +0300

    exp dj_templ

--- article/views.py
+++ article/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("article")
+    return render(
+        request,
+        "article/index.html",
+        context = {"name": "article"}
+    )
